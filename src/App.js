18ff44ca8f311014f10b81f90e7a3fd3410707e2
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-04-11 23:55:24 +0600

    Created Die component and styled it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Die from "./components/Die";
+
 function App() {
   return (
     <div className="App">
-      <div className="main"></div>
+      <div className="main">
+        <div className="die-container">
+          <Die value="1" />
+          <Die value="2" />
+          <Die value="3" />
+          <Die value="4" />
+          <Die value="5" />
+          <Die value="6" />
+          <Die value="7" />
+          <Die value="8" />
+          <Die value="9" />
+          <Die value="1" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,9 @@
+const Die = (props) => {
+  return (
+    <div className="die-face">
+      <h2>{props.value}</h2>
+    </div>
+  );
+};
+
+export default Die;
